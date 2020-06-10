27aa46b3a29f10f61c11afee1d7bfa6f76729775
ryohei udagawa <r.udagawa1995@gmail.com>
2020-06-10 16:52:58 +0900

    update: matter検索のテスト追加

--- spec/factories/matters.rb
+++ spec/factories/matters.rb
@@ -1,8 +1,22 @@
 FactoryBot.define do
-  factory :matter do
-      title { 'test_title' }
-      content { 'test_content' }
-      start_date { '2020-06-09' }
-      completion_date { '2020-06-10' }
+  factory :test1, class: Matter do
+      title { 'test1_title' }
+      content { 'test1_content' }
+      status { '未着手' }
+      priority { 2 }
+  end
+
+  factory :test2, class: Matter do
+      title { 'test2_title' }
+      content { 'test2_content' }
+      status { '工事中' }
+      priority { 0 }
+  end
+
+  factory :test3, class: Matter do
+      title { 'test3_title' }
+      content { 'test3_content' }
+      status { '工事完了' }
+      priority { 1 }
   end
 end
