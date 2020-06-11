c8d44b5130a4155af2028692372376a58dc85bee
ryohei udagawa <r.udagawa1995@gmail.com>
2020-06-11 15:41:10 +0900

    add: userの詳細追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }#current_passwordを入れず、編集する
   root to: 'matters#index'
+
+  devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }#current_passwordを入れず、編集する
+  get "users/show" => "users#show"
+
   resources :matters
+  
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
