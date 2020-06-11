3a10ff294743de2280bc9ee1a46a00e71a6962f0
ryohei udagawa <r.udagawa1995@gmail.com>
2020-06-11 15:28:22 +0900

    add: deviseの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }#current_passwordを入れず、編集する
   root to: 'matters#index'
   resources :matters
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
