2761f86bb08466d0723d979d76a4177e95fb53a0
ryohei udagawa <r.udagawa1995@gmail.com>
2020-06-18 21:21:25 +0900

    testの修正 usercontrollerのif_not_admin削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,6 @@
 class UsersController < ApplicationController
-  before_action :if_not_admin
 
   def show
     @user = current_user
-  end
-
-  private
-  def if_not_admin
-    redirect_to root_path unless current_user.admin?
   end
 end
