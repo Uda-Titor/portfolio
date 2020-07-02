5a165c014f18c450ed100dfb195401a712a781f9
ryohei udagawa <r.udagawa1995@gmail.com>
2020-07-02 21:49:14 +0900

    update:user formアイコンプレビュー機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,6 @@ class ApplicationController < ActionController::Base
     # サインアップ時にnameのストロングパラメータを追加
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     # アカウント編集の時にnameとprofileのストロングパラメータを追加
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :user_image, :phone_number, :user_address, :remark])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :user_image, :phone_number, :user_address, :remark, :remove_user_image])
   end
 end
