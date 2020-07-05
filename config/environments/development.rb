ad492a28fd02ac643637313f13e27033d1926483
ryohei udagawa <r.udagawa1995@gmail.com>
2020-07-05 14:39:09 +0900

    update: navの通知ボタン修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
-  # Highlight code that triggered database queries in logs.
+  # Highcolor code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
   # Debug mode disables concatenation and preprocessing of assets.
