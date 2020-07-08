dd8315d51bb57df7e76f92b0d1417b48b737ab1a
ryohei udagawa <r.udagawa1995@gmail.com>
2020-07-08 22:11:23 +0900

    update:notificationのコントローラー消去

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,5 +1,2 @@
-class NotificationsController < ApplicationController
-  def index
-    @notifications = current_user.passive_notifications.page(params[:page]).per(20)
-  end
+class NotificationsController < ApplicationControllerz
 end
