0b6d988cff81de7e92e6c87b26f2a30ee9dbbb5b
ryohei udagawa <r.udagawa1995@gmail.com>
2020-07-09 18:43:23 +0900

    update:labellingのモデルテスト追加

--- app/models/label.rb
+++ app/models/label.rb
@@ -1,4 +1,8 @@
 class Label < ApplicationRecord
+  validates :name, presence: true, uniqueness: true, length: { maximum: 10 }
+
   has_many :labellings, dependent: :destroy
   has_many :tasks, through: :labellings
+
+
 end
