a85d7409105e0c9afb76fa43dfcfbeb297f26a9f
ryohei udagawa <r.udagawa1995@gmail.com>
2020-07-09 22:40:34 +0900

    update:notificationのmodelテスト追加

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -9,23 +9,27 @@ RSpec.describe Notification, type: :model do
       FactoryBot.create(:comment)
     end
     it "有効な状態であること" do
-      notification = Notification.new(visitor_id: 1, visited_id: 2, matter_id: 1, comment_id: 1, action: 'comment', checked: false, admin_checked: false)
+      notification = Notification.new(visitor_id: 1, visited_id: 2, matter_id: 1, comment_id: 1, action: 'comment', checked: true, admin_checked: true)
       expect(notification).to be_valid
     end
     it "visitor_idがnilならば無効な状態であること" do
-      notification = Notification.new(visitor_id: nil, visited_id: 2, matter_id: 1, comment_id: 1, action: 'comment', checked: false, admin_checked: false)
+      notification = Notification.new(visitor_id: nil, visited_id: 2, matter_id: 1, comment_id: 1, action: 'comment', checked: true, admin_checked: true)
       expect(notification).not_to be_valid
     end
     it "visited_idがnilならば無効な状態であること" do
-      notification = Notification.new(visitor_id: 1, visited_id: nil, matter_id: 1, comment_id: 1, action: 'comment', checked: false, admin_checked: false)
+      notification = Notification.new(visitor_id: 1, visited_id: nil, matter_id: 1, comment_id: 1, action: 'comment', checked: true, admin_checked: true)
+      expect(notification).not_to be_valid
+    end
+    it "matter_idがnilならば無効な状態であること" do
+      notification = Notification.new(visitor_id: 1, visited_id: 2, matter_id: nil, comment_id: 1, action: 'comment', checked: true, admin_checked: true)
       expect(notification).not_to be_valid
     end
     it "actionがnilならば無効な状態であること" do
-      notification = Notification.new(visitor_id: 1, visited_id: nil, matter_id: 1, comment_id: 1, action: nil, checked: false, admin_checked: false)
+      notification = Notification.new(visitor_id: 1, visited_id: nil, matter_id: 1, comment_id: 1, action: nil, checked: true, admin_checked: true)
       expect(notification).not_to be_valid
     end
     it "checkedがnilならば無効な状態であること" do
-      notification = Notification.new(visitor_id: 1, visited_id: nil, matter_id: 1, comment_id: 1, action: nil, checked: nil, admin_checked: false)
+      notification = Notification.new(visitor_id: 1, visited_id: nil, matter_id: 1, comment_id: 1, action: nil, checked: nil, admin_checked: true)
       expect(notification).not_to be_valid
     end
     it "admin_checkedがnilならば無効な状態であること" do
