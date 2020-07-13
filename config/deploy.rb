c48f7fad144c740d108d909583102fe50a8d80e2
ryohei udagawa <r.udagawa1995@gmail.com>
2020-07-13 11:26:07 +0900

    update:deployerrorの修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -52,6 +52,25 @@ namespace :deploy do
     end
   end
 end
+
+set :bundle_flags,      '--quiet' # this unsets --deployment, see details in config_bundler task details
+set :bundle_path,       nil
+set :bundle_without,    nil
+
+namespace :deploy do
+  desc 'Config bundler'
+  task :config_bundler do
+    on roles(/.*/) do
+      within release_path do
+        execute :bundle, :config, '--local deployment true'
+        execute :bundle, :config, '--local without "development test"'
+        execute :bundle, :config, "--local path #{shared_path.join('bundle')}"
+      end
+    end
+  end
+end
+
+before 'bundler:install', 'deploy:config_bundler'
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
