37eea46e02d5849197d2c59d9a91a8a1c8f7753a
ryohei udagawa <r.udagawa1995@gmail.com>
2020-07-13 13:30:45 +0900

    update:deploy error

--- db/migrate/20200708005916_rename_user_address_column_to_user_place.rb
+++
@@ -1,9 +0,0 @@
-class RenameUserAddressColumnToUserPlace < ActiveRecord::Migration[5.2]
-  def up
-    rename_column :users, :user_place, :user_place
-  end
-
-  def down
-    rename_column :users, :user_place, :user_place
-  end
-end
