c1f9bef462c5e108c242337fdad1073b9d9ad760
ryohei udagawa <r.udagawa1995@gmail.com>
2020-07-19 18:00:01 +0900

    update:mailer smtpをgmailを使い設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,7 +4,21 @@ Rails.application.configure do
   config.cache_classes = false
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :letter_opener_web
+  #送信方法を指定（この他に:sendmail/:file/:testなどがあります)
+  config.action_mailer.delivery_method = :smtp
+  #送信方法として:smtpを指定した場合は、このconfigを使って送信詳細の設定を行います
+  config.action_mailer.smtp_settings = {
+    #gmail利用時はaddress,domain,portは下記で固定
+    address:"smtp.gmail.com",
+    domain: 'gmail.com',
+    port:587,
+    #gmailのユーザアカウント（xxxx@gmail.com)※念のため、credentials.yml.enc行き
+    user_name: Rails.application.credentials.dig(:gmail, :user_name),
+    #gmail２段階認証回避のためにアプリケーションでの利用パスワードを取得、必ずcredentials.yml.endに設定を！！
+    password: Rails.application.credentials.dig(:gmail, :password),
+    #パスワードをBase64でエンコード
+    authentication: :login
+  }
 
   # Do not eager load code on boot.
   config.eager_load = false
